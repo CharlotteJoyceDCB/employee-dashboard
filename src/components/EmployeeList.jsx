bc983daff61165ba0525947cef4e465b8a568682
CharlotteJoyce <charlottejdcb@gmail.com>
2025-01-05 23:07:39 +0800

    Fixed filename casing issues

--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import EmployeeCard from './EmployeeCard';
 
-const EmployeeList = ({ employees }) => {
+const EmployeeList = ({ employees , onEdit}) => {
   return (
     <div className="employee-list">
       <h2>Employee List</h2>
       {employees.map((employee) => (
-        <EmployeeCard key={employee.id} employee={employee} />
+        <div key={employee.id}>
+            <EmployeeCard employee={employee} />
+            <button onClick={() => onEdit(employee)}>Edit</button>
+        </div>
       ))}
     </div>
   );
