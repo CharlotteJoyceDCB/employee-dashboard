4de81a56ce7454f4c945807e0f810eb410bbb2e9
CharlotteJoyce <charlottejdcb@gmail.com>
2025-01-05 20:09:20 +0800

    Added data to EmployeeList component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import React, { useState } from 'react';
+import EmployeeList from './components/EmployeeList';
+
 const App = () => {
+  const [employees, setEmployees] = useState([
+    { id: 1,
+      name: 'John Doe',
+      position: 'Software Engineer'
+    },
+    { id: 2,
+      name: 'Juan Dela Cruz',
+      position: 'Backend Developer'
+    },
+  ]);
+
   return (
-    <div>
+    <div className="app">
       <h1>Welcome to Employee Management Dashboard</h1>
+      <EmployeeList employees={employees}/>
     </div>
   );
 };
---
+++ src/components/EmployeeList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const EmployeeList = ({ employees }) => {
+  return (
+    <div className="employee-list">
+      <h2>Employee List</h2>
+      <ul>
+        {employees.map((employee) => (
+          <li key={employee.id}>
+            {employee.name} - {employee.position}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default EmployeeList;
